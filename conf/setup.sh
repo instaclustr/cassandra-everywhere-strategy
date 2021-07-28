2b2050339e32b4b7dab3fd1b654766bb88927235
Stefan Miklosovic <smiklosovic@apache.org>
2021-07-28 10:40:29 +0200

    build fixes

--- conf/setup.sh
+++ conf/setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-whereis pip
-whereis wget
-whereis zip
-whereis unzip
-whereis python
+sudo apt-get update
+sudo apt-get --allow-downgrades --allow-remove-essential --allow-change-held-packages install libev4 libev-dev
 
 pip3.9 install ccm && wget https://github.com/zegelin/ccm-extensions/archive/refs/heads/master.zip && unzip master.zip && cd ccm-extensions-master && python3.9 setup.py install
--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-ip link add name loop1 type dummy
-ip link set loop1 up
-ip addr add 127.0.0.2/8 dev loop1
-
 # ssltabledump not available on 2.2.x
 #python3 ccm-test.py -j ../cassandra-2.2/target/everywhere-strategy-cassandra-2.2-1.0.0.jar 2.2.18
-# circleci machine is running with Java 11 and 3.0 nor 3.11 is not compatible with that
-#python3 ccm-test.py -j ../cassandra-3.0/target/everywhere-strategy-cassandra-3.0-1.0.0.jar 3.0.24
-#python3 ccm-test.py -j ../cassandra-3.11/target/everywhere-strategy-cassandra-3.11-1.0.0.jar 3.11.10
+python3 ccm-test.py -j ../cassandra-3.0/target/everywhere-strategy-cassandra-3.0-1.0.0.jar 3.0.24
+python3 ccm-test.py -j ../cassandra-3.11/target/everywhere-strategy-cassandra-3.11-1.0.0.jar 3.11.10
 python3 ccm-test.py -j ../cassandra-4/target/everywhere-strategy-cassandra-4.0-1.0.0.jar 4.0.0
