e7a6f0510fa1e3d3acb76f054cd2df1579391c64
Stefan Miklosovic <smiklosovic@apache.org>
2022-12-14 10:56:16 +0100

    next

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -5,4 +5,4 @@
 python3 ccm-test.py -j ../cassandra-3.0/target/everywhere-strategy-cassandra-3.0-1.0.0.jar 3.0.26
 python3 ccm-test.py -j ../cassandra-3.11/target/everywhere-strategy-cassandra-3.11-1.0.0.jar 3.11.14
 python3 ccm-test.py -j ../cassandra-4/target/everywhere-strategy-cassandra-4.0-1.0.0.jar 4.0.7
-python3 ccm-test.py -j ../cassandra-4/target/everywhere-strategy-cassandra-4.1-1.0.0.jar 4.1.0
+python3 ccm-test.py -j ../cassandra-4.1/target/everywhere-strategy-cassandra-4.1-1.0.0.jar 4.1.0
